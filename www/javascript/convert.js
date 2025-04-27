1b48130d72f70da78aa2a5702e561966e935e87e
thisisaaronland <thisisaaronland@localhost>
2025-04-26 22:48:12 -0700

    remove submit button

--- www/javascript/convert.js
+++ www/javascript/convert.js
@@ -8,7 +8,6 @@ window.addEventListener("load", function load(event){
     var quantity_el = document.getElementById("quantity");
     var from_el = document.getElementById("from");
     var to_el = document.getElementById("to");            
-    var submit_el = document.getElementById("submit");
 
     const wasm_uri = location.pathname + "wasm/convert.wasm";
     
@@ -37,14 +36,12 @@ window.addEventListener("load", function load(event){
 		const q = quantity_el.value;
 
 		if (! q){
-		    submit_el.setAttribute("disabled", "disabled");
 		    return;
 		}
 
 		const units = units_map[q];
 
 		if (! units){
-		    submit_el.setAttribute("disabled", "disabled");
 		    return;
 		}
 
@@ -82,24 +79,27 @@ window.addEventListener("load", function load(event){
 		    const to_v = to_el.value;
 
 		    if (! v){
-			feedback_el.innerText = "Missing value.";
-			submit_el.setAttribute("disabled", "disabled");		    			
 			return false;
 		    }
 		    
 		    if ((! from_v) || (! to_v)){
-			feedback_el.innerText = "One or more missing units.";			
-			submit_el.setAttribute("disabled", "disabled");		    			
 			return false;
 		    }
 
 		    if (from_v == to_v){
-			feedback_el.innerText = "Units are the same.";						
-			submit_el.setAttribute("disabled", "disabled");		    			
 			return false;
 		    }
+
+		    console.log("Convert", v, from_v, to_v);
 		    
-		    submit_el.removeAttribute("disabled");
+		    convert_units(v, from_v, to_v).then((rsp) => {
+			const new_v = JSON.parse(rsp);
+			result_el.innerText = new_v.toFixed(2);
+		    }).catch((err) => {
+			feedback_el.innerText = "Failed to convert units, " + err;
+			console.error("Failed to convert units", err);
+		    });
+		    
 		    return false;
 		};
 		
@@ -107,46 +107,6 @@ window.addEventListener("load", function load(event){
 		to_el.onchange = onchange_func;		
 	    };
 
-	    submit_el.onclick = function(e){
-
-		feedback_el.innerHTML = "";
-		result_el.innerHTML = "";		
-		
-		const v = value_el.value;
-		const from_v = from_el.value;
-		const to_v = to_el.value;
-
-		if (! v){
-		    feedback_el.innerText = "Missing value.";
-		    submit_el.setAttribute("disabled", "disabled");		    			
-		    return false;
-		}
-		
-		if ((! from_v) || (! to_v)){
-		    feedback_el.innerText = "One or more missing units.";
-		    submit_el.setAttribute("disabled", "disabled");		    			
-		    return false;
-		}
-		
-		if (from_v == to_v){
-		    feedback_el.innerText = "Units are the same.";
-		    submit_el.setAttribute("disabled", "disabled");		    			
-		    return false;
-		}
-
-		console.log("Convert", v, from_v, to_v);
-
-		convert_units(v, from_v, to_v).then((rsp) => {
-		    const new_v = JSON.parse(rsp);
-		    result_el.innerText = new_v.toFixed(2);
-		}).catch((err) => {
-		    feedback_el.innerText = "Failed to convert units, " + err;
-		    console.error("Failed to convert units", err);
-		});
-		
-		return false;
-	    };
-	    
 	}).catch((err) => {
 	    throw err
 	})
